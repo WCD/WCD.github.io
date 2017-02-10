08e394ecd69cc85b289fe608f27f6a4c7484c61b
Hudson Green <hudsongtv@outlook.com>
2017-02-10 13:40:11 -0800

    Add files via upload

---
+++ js/pageLoad.js
@@ -0,0 +1,7 @@
+function calcHeight() {
+	//find the height of the internal page
+	var the_height = document.getElementById('page-content').contentWindow.document.body.scrollHeight;
+
+	//change the height of the iframe
+	document.getElementById('page-content').height = the_height;
+}
