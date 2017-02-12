efaf52b1b891b52796c32413619a505b98aefda0
Hudson Green <hudsongtv@outlook.com>
2017-02-11 17:22:26 -0800

    Update pageLoad.js

--- js/pageLoad.js
+++ js/pageLoad.js
@@ -20,6 +20,8 @@ $(document).ready(function() {
 			
 			if(file != 'home') {
 				window.history.pushState({id: file} + '.html', file + '.html', file);
+			} else {
+				window.history.pushState({id: file} + '.html', file + '.html', '');
 			}
 			
 			return false;
