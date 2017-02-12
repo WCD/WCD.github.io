2e7a84b3ceef4a921ac657ae39b5e60303d2403f
Hudson Green <hudsongtv@outlook.com>
2017-02-11 16:21:26 -0800

    Add files via upload

--- js/pageLoad.js
+++ js/pageLoad.js
@@ -9,3 +9,23 @@ function calcHeight() {
 $(function() {
 	$("#headerSlideshow").carousel();
 });
+
+var container = document.querySelector('#content');
+
+$( document ).ready(function() {
+	$( ".loadContent a" ).click(function() {
+		var file = $(this).data("file");
+		$('#content').load(file, function(){
+			window.history.pushState({id: file}, file, file);
+			return false;
+		})
+	});
+});
+
+container.addEventListener('click', function(e) {
+  if (e.target != e.currentTarget) {
+    e.preventDefault();
+    // e.target is the image inside the link we just clicked.
+  }
+  e.stopPropagation();
+}, false);
